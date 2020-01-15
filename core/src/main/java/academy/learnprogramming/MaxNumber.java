d1a3993d4a2c267a20c8c6f0a27538f57558c604
lswarss <ls.warss98@gmail.com>
2020-01-15 22:08:37 +0100

    Initial commit

---
+++ core/src/main/java/academy/learnprogramming/MaxNumber.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD}) // that indicates context in which it is applicable
+@Retention(RetentionPolicy.RUNTIME) //it indicates how long the annotation types will stay
+@Qualifier //it show us that this can be used as qualifier
+public @interface MaxNumber {
+
+}
