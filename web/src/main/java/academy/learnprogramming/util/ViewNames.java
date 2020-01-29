85f4ccbde180398b10bc36fe1d18b3349b2adebf
lswarss <ls.warss98@gmail.com>
2020-01-29 14:02:49 +0100

    Added thymeleaf and clean up the html code

---
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
+    // == constructors ==
+    private ViewNames() {}
+}
